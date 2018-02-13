e2c836eb03e65f2671fb160b32fe008a436080c2
Jen <jen@Jens-MacBook-Pro.local>
2018-02-13 15:32:30 +1100

    I'm so glad I asked for help to change the background color theme from a peer last week because it means I can do something...

--- art.js
+++ art.js
@@ -1,11 +1,12 @@
 console.log("Hello!");
+//commit 1
 
 var $setColor = $('.set-color'); 
 var $input = $('.colorTextInput');
-var $div = $('div')
+var $brush = $('.brush');
 
 
-$div.append($input);
+//$div.append($input);
 
 /*
 $setColor.addEventListener('click', changeColor);
@@ -24,10 +25,21 @@ var backgroundChange = false;
 
 $setColor.on('click', function(){
 	if (!backgroundChange) {
-		$div.attr('class', 'new-color');
+		$brush.attr('class', 'new-color');
 		backgroundChange = true;
 	} else {
-		$div.removeClass();
+		$brush.removeClass();
 		backgroundChange = false;
 	}
-})
+});
+
+//commit 3
+var $body = $('body');
+
+for (var x = 0; x <= 100; x++) {
+	var $newDiv = $('<div>');
+	$newDiv.attr('class', 'new-containers');
+	$body.append($newDiv);
+
+};
+
